fa4a33bb68278c9bfbcb91c76eb969d611181131
portlek <hsndmrts_@outlook.com>
2019-07-24 19:16:44 +0300

    test

---
+++ src/main/test/io/github/portlek/versionmatched/VersionClassTest.java
@@ -0,0 +1,42 @@
+package io.github.portlek.versionmatched;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionClassTest {
+
+    private static final String MC_VERSION = "1_14_R1";
+
+    @Test
+    void getVersionClass() {
+        final VersionClass<ITest> versionClass = new VersionClass<>(Test1_14_R1.class);
+        new Assertion<>(
+            "Cannot get the class",
+            versionClass.getVersionClass(),
+            new IsEqual<>(Test1_14_R1.class)
+        );
+    }
+
+    @Test
+    void match() {
+        final VersionClass<ITest> versionClass = new VersionClass<>(Test1_14_R1.class);
+        new Assertion<>(
+            "Cannot get the class",
+            versionClass.match(MC_VERSION),
+            new IsEqual<>(Test1_13_R2.class)
+        );
+    }
+
+    private interface ITest {
+    }
+
+    private class Test1_14_R1 implements ITest {
+    }
+
+    private class Test1_13_R2 implements ITest {
+    }
+
+}
